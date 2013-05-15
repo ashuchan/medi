fff2eb4c9d3790c4c731941ac0eaacf790becd3b
Ashu Saxena <ashusaxe007@gmail.com>
2013-05-15 15:07:44 -0700

    changed again
    Final change for the home page

--- scripts/main.js
+++ scripts/main.js
@@ -3,6 +3,8 @@ var width = $(window).width();
 
 var current=1;
 
+var currentAd=1;
+
 function slideShow(){
 	var cId = "#slide"+current;
 	$(cId).animate({ opacity: 0 }, 2000);
@@ -11,5 +13,22 @@ function slideShow(){
 		current=1;
 	cId = "#slide"+current;
 	$(cId).animate({ opacity: 1 }, 2000);
+	
+	//animation for ads
+	
+	cId = "#ad"+currentAd;
+	$(cId).animate({ opacity: 0 }, 2000);
+	currentAd++;
+	if(currentAd>2)
+		currentAd=1;
+	cId = "#ad"+currentAd;
+	$(cId).animate({ opacity: 1 }, 2000);
+	
 	setTimeout(slideShow,6000);
+}
+
+function scrollToSignUp(){
+	$('html,body').animate({
+		scrollTop: $('#signUp').offset().top
+		}, 1000);
 }
